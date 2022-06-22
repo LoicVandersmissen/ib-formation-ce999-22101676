0ebee6b7d7a08bf9bfc5e52f505e24dd07b3e3b6
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-06-22 17:22:48 +0200

    CE999 Day 3

--- js/demo_bases.js
+++ js/demo_bases.js
@@ -82,10 +82,83 @@ document.write('<h1>' + code + ' - ' + desc + '</h1>');
 document.write('<p>Durée : ' + duration + ' heures</p>');
 document.write('<p>Public : ' + attendance + ' participants</p>');
 
+/**
+ * Variables composées ou array (tableau)
+ */
+let matrix = [];
+console.log(typeof matrix);
 
+let course = [
+    'CE999',
+    "Développement web",
+    5,
+    true
+];
+course[10] = 'Lesly';
+course[4] = ['HTML5', 'CSS3', 'JS'];
+course.push(attendance); // ajout en fin de tableau (file)
+course.unshift('2022-06-20T09:30:00') // ajout en début de tableau (pile)
+console.log(course);
 
+/**
+ * Objets globaux : String, Number, Boolean, Array, Date, Regexp, Math, Function,..., Object
+ */
 
+// String
+let p1 = 'Margaux';
+let p2 = new String('Margaux');
+console.log(p1 == p2); // compare les valeurs
+console.log(p1 === p2); // compare strictement les valeurs et les types
+console.log(typeof p1);
+console.log(typeof p2);
 
+// Boolean
+let b1 = false; // false
+let b2 = new Boolean(false); // false
+let b3 = ''; // false
+let b4 = 0; // false
+let b5 = null; // false
+let b6 = 'Loïc'; // true
 
+// Math (non instanciable)
+console.log(Math.PI);
+console.log(Math.round(Math.PI)); // entier le plus proche
+console.log(Math.ceil(5.00000001)); // 6
+console.log(Math.floor(5.9999999991)); // 5
+console.log(Math.PI.toFixed(5));
+console.log(Math.random());
+console.log(Math.max(2000, 100, 30, 0, -20));
+
+let numbers = [2000, 100, 30, 0, -20];
+console.log(Math.min.apply(null, numbers));
+
+// Date : timestamp (à partir du 01/01/1970)
+console.log(new Date());
+console.log(new Date(-123456789));
+console.log(new Date(123456789));
+console.log(new Date(1998, 6, 12, 22, 45, 01)); // 0=Janvier, 1=Février,..., 11=Décembre
+console.log(new Date('7/2/2000 22:45:02')); // 2 juillet 2000
+console.log(new Date('December 18, 2022 20:45:04 GMT+0200'));
+console.log(new Date('2018-07-15T12:45:03.123')); // universel car ISO
+
+/**
+ * Structures de contrôle
+ */
+
+// if
+// si nb de participants est > 10 alors plus de stagiaires
+let part = 9;
+if (part < 4) {
+    console.info('Il reste encore beaucoup de places.');
+} else if (part < 7) {
+    console.info('Il reste encore quelques places.');
+} else if (part < 10) {
+    console.info('Il ne reste plus beaucoup de places.');
+} else {
+    console.warn('Attention le nb de participants est dépassé.');
+}
+
+// switch
+// conversion du if en switch
 
 
