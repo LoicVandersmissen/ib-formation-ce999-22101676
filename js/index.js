80ecd0c8205dd9885d27d98e69162fa032c65352
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-06-23 15:36:14 +0200

    CE999 Day 4

--- js/index.js
+++ js/index.js
@@ -39,5 +39,47 @@ function getMembers() {
     }
 }
 
+/**
+ * Procédure qui va remplir la section TEAM avec un tableau d'objets littéraux
+ */
+
+function getUsers() {
+    let section, article, img, div, h2, p, a;
+
+    // Pointe sur la section TEAM
+    section = document.getElementById('team');
+
+    // Pour chaque membre du tableau USERS
+    for (let i = 0; i < users.length; i++) {
+        // Crée la balise ARTICLE
+        article = document.createElement('article');
+        article.setAttribute('class', 'member ' + (users[i].gender === 'male' ? 'boy' : 'girl'));
+        section.appendChild(article);
+        // Crée la balise IMG
+        img = document.createElement('img');
+        img.setAttribute('src', users[i].image);
+        img.setAttribute('alt', 'fiche de ' + users[i].firstName);
+        article.appendChild(img);
+        // Crée la balise DIV
+        div = document.createElement('div')
+        article.appendChild(div);
+        // Crée la balise H2
+        h2 = document.createElement('h2');
+        h2.textContent = users[i].firstName + ' ' + users[i].lastName.toUpperCase();
+        div.appendChild(h2);
+        // Crée la balise P
+        p = document.createElement('p');
+        p.textContent = users[i].university;
+        div.appendChild(p)
+        // Crée la balise A
+        a = document.createElement('a');
+        a.setAttribute('href', 'https://www.google.fr/search?q=' + users[i].firstName);
+        a.setAttribute('target', '_blank');
+        a.textContent = 'En savoir plus sur ' + users[i].firstName;
+        div.appendChild(a);
+    }
+}
+
 // Appelle la fonction
-getMembers();
+// getMembers();
+getUsers();
